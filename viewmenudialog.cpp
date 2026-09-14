2b09791f2a31ddb05d5dd2c78fd7d88d496a1d10
Chris Olson <chris.m.olson@gmail.com>
2026-09-14 12:55:29 -0500

    In progress ... work harder

--- viewmenudialog.cpp
+++ viewmenudialog.cpp
@@ -6,16 +6,16 @@ ViewMenuDialog::ViewMenuDialog(QWidget *parent)
     : QDialog(parent)
     , ui(new Ui::ViewMenuDialog) {
     ui->setupUi(this);
-    connect(ui->z0_5, &QPushButton::clicked, this, [this](bool) { accepted(); emit Sheet::ref().action0_5();});
-    connect(ui->z0_5, &QPushButton::clicked, this, [this](bool) { accepted(); emit Sheet::ref().action0_5();});
-    connect(ui->z0_5, &QPushButton::clicked, this, [this](bool) { accepted(); emit Sheet::ref().action0_5();});
-    connect(ui->z0_5, &QPushButton::clicked, this, [this](bool) { accepted(); emit Sheet::ref().action0_5();});
-    connect(ui->z0_5, &QPushButton::clicked, this, [this](bool) { accepted(); emit Sheet::ref().action0_5();});
-    connect(ui->z0_5, &QPushButton::clicked, this, [this](bool) { accepted(); emit Sheet::ref().action0_5();});
-    connect(ui->z0_5, &QPushButton::clicked, this, [this](bool) { accepted(); emit Sheet::ref().action0_5();});
-    connect(ui->z0_5, &QPushButton::clicked, this, [this](bool) { accepted(); emit Sheet::ref().action0_5();});
-    connect(ui->z0_5, &QPushButton::clicked, this, [this](bool) { accepted(); emit Sheet::ref().actionZoom_In();});
-    connect(ui->z0_5, &QPushButton::clicked, this, [this](bool) { accepted(); emit Sheet::ref().actionZoom_Out();});
+    connect(ui->z0_5,     &QPushButton::clicked, this, [this](bool) { accepted(); emit Sheet::ref().action0_5; });
+    connect(ui->z0_75,    &QPushButton::clicked, this, [this](bool) { accepted(); emit Sheet::ref().action0_5(); });
+    connect(ui->z0_9,     &QPushButton::clicked, this, [this](bool) { accepted(); emit Sheet::ref().action0_5(); });
+    connect(ui->z1_0,     &QPushButton::clicked, this, [this](bool) { accepted(); emit Sheet::ref().action0_5(); });
+    connect(ui->z1_25,    &QPushButton::clicked, this, [this](bool) { accepted(); emit Sheet::ref().action0_5(); });
+    connect(ui->z1_5,     &QPushButton::clicked, this, [this](bool) { accepted(); emit Sheet::ref().action0_5(); });
+    connect(ui->z2_0,     &QPushButton::clicked, this, [this](bool) { accepted(); emit Sheet::ref().action0_5(); });
+    connect(ui->z3_0,     &QPushButton::clicked, this, [this](bool) { accepted(); emit Sheet::ref().action0_5(); });
+    connect(ui->Zoom_In,  &QPushButton::clicked, this, [this](bool) { accepted(); emit Sheet::ref().actionZoom_In();});
+    connect(ui->Zoom_Out, &QPushButton::clicked, this, [this](bool) { accepted(); emit Sheet::ref().actionZoom_Out();});
 }
 
 ViewMenuDialog::~ViewMenuDialog() {
